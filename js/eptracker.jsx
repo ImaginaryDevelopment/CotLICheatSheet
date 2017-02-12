dee36602d37280f624a7eec1a42fccedfeafa74e
ImaginaryDevelopment <imaginarydevelopment@googlemail>
2017-02-12 13:49:56 -0500

    ep tracking appears to work

--- js/eptracker.jsx
+++ js/eptracker.jsx
@@ -1,5 +1,95 @@
 // data is in window.jsonData
 var loggedStorageFailure = false;
+const isDefined = function(o){
+    return typeof(o) !== 'undefined' && o !== null;
+};
+const trim = function(s) {
+    return s.trim();
+};
+const debounce = (function(){
+        var timer = 0;
+        return (function(callback, ms){
+            if(typeof(callback) !== "function")
+                throw callback;
+            // this method does not ever throw, or complain if passed an invalid id
+            clearTimeout(timer);
+            timer = setTimeout(callback,ms); //setTimeout(callback,ms);
+        });
+    })();
+let debounceChange = function (callback, e, ...args) {
+    if(!isDefined(callback)){
+        console.info('no callback for debounceChange',e.target, typeof(callback),callback);
+        return;
+    }
+    e.persist();
+    args.unshift(e.target.value);
+    debounce(() => callback(...args), 500);
+};
+const flattenArrays = (a,b) => {
+        var isAa = Array.isArray(a),isAb = Array.isArray(b);
+        if(isAa && !isAb){
+            let result = a.slice(0);
+            result.push(b);
+            return result;
+        }
+        if(isAa && isAb){
+            return a.concat(b);
+        }
+        if(!isAa && isAb){
+            let result = [a].concat(b);
+            return result;
+        }
+        return [a,b];
+    };
+// otherClasses: allows/adapts to inputs of type string or array
+const addClasses = (defaultClasses=[], otherClasses=[]) =>{
+    var unwrappedClasses = defaultClasses.reduce(flattenArrays,[]);
+    unwrappedClasses = otherClasses.reduce(flattenArrays,[]).concat(unwrappedClasses);
+    return unwrappedClasses.filter(isDefined).map(trim).join(' ').trim();
+};
+var TextInput2 = props =>
+(<input
+        name={props.name}
+        className={addClasses(['form-control'],props.className)}
+        type={props.type}
+        value={props.value}
+        defaultValue={props.defaultValue}
+        onChange={ e =>
+            {
+            if(props.onControlledChange){
+                props.onControlledChange(e);
+            }
+            return debounceChange(props.onChange,e)
+          }
+        }
+        onBlur={props.onBlur}
+        {...props.spread} />);
+var TextInputUnc = React.createClass({
+  getInitialState(){
+    return {value:this.props.value};
+  },
+  componentWillReceiveProps(nextProps){
+    if(this.props.value !== nextProps.value && this.props.id !== nextProps.id){
+      this.setState({value:nextProps.value});
+    }
+  },
+  render(){
+    var props = this.props;
+    var state = this.state;
+    return (<TextInput2 
+          name={props.name} 
+          defaultValue={props.defaultValue} 
+          value={state.value? state.value : ''}
+          type={props.type}
+          min={props.min}
+          onControlledChange={e => this.setState({value: e.target.value})}
+          onChange={e => props.onChange(e)}
+          onBlur={e => e.target.value === '' ? {} : e.target.value = (+e.target.value)}
+          spread={props.spread}
+      />
+      );
+  }
+});
 function getIsLocalStorageAvailable() {
   if (typeof(localStorage) !== 'undefined' && (typeof(localStorage.setItem) === 'function') && typeof(localStorage.getItem) === 'function'){
     return true;
@@ -98,18 +188,20 @@ var CruTagRow = React.createClass({
           tags.push(imgTag);
         }
       });
+      var isOwned = this.props.owned || cru.slot == cru.id && cru.slot < 21;
       var owned = null;
       var formation = null;
+      var epBox = this.props.epMode && isOwned ? <TextInputUnc type="number" min="0" onChange={this.props.onEpChange} value={this.props.enchantmentPoints} /> : null;
       if(this.props.mode === "mine" && this.props.isFormationMode){
         if(this.props.formationIds[cru.slot]){
           console.log('formation',this.props.formationIds,cru);
         }
-        formation = (<td key="formation"><CheckBox checked={this.props.formationIds[cru.slot] == cru.id ? true: false} onChange={this.props.onFormationChange} /></td>);
+        formation = (<td key="formation"><CheckBox checked={this.props.formationIds[cru.slot] == cru.id ? true: false} onChange={this.props.onFormationChange} />{epBox}</td>);
       } else if (this.props.mode === "mine" && !this.props.isFormationMode){
         if(cru.slot == cru.id && cru.slot < 21){
-          owned = (<td key="owned"><CheckBox checked={true} readonly={true} disabled={true} /></td>);
+          owned = (<td key="owned"><CheckBox checked={true} readonly={true} disabled={true} />{epBox}</td>);
         } else {
-          owned = (<td key="owned"><CheckBox checked={this.props.owned} onChange={this.props.onOwnedChange} /></td>);
+          owned = (<td key="owned"><CheckBox checked={this.props.owned} onChange={this.props.onOwnedChange} />{epBox}</td>);
         }
       }
       var link = cru.link && cru.link.indexOf('/') < 0 ? (self.props.wikibase + cru.link) 
@@ -135,7 +227,7 @@ function padLeft(nr, n, str){
 var CruTagGrid = React.createClass({
   getInitialState:function(){
     // json.stringify this whole thing to make input/html5 storage data
-    var init = readIt("cruTagGrid", {slotSort:"up",mode:"",filterOwned:false,ownedCrusaderIds:[], formation:null, filterTags:{}, formationIds:{}});
+    var init = readIt("cruTagGrid", {slotSort:"up",mode:"",epMode:false,enchantmentPoints:{},filterOwned:false,ownedCrusaderIds:[], formation:null, filterTags:{}, formationIds:{}});
     var toRemove=[];
     init.ownedCrusaderIds.sort();
     var x = init.ownedCrusaderIds;
@@ -184,6 +276,24 @@ var CruTagGrid = React.createClass({
   onFormationClick: function(){
     var stateMods = {formation:this.state.formation != null ? null : "formation"};
     console.log('formationClick', stateMods);
+    this.setState(stateMods);
+  },
+  onEpClick: function(){
+    var stateMods = {epMode:this.state.epMode? false:true};
+    console.log('epClick',stateMods);
+    this.setState(stateMods);
+  },
+  onEpChange:function(crusaderId, epValue){
+    console.log('onEpChange',arguments);
+    var oldEp = this.state.enchantmentPoints;
+    var newEp = {};
+    for(var attr in oldEp){
+      if (oldEp.hasOwnProperty(attr)) {
+        newEp[attr] = oldEp[attr];
+      }
+    }
+    newEp[crusaderId] = epValue;
+    var stateMods = {enchantmentPoints:newEp};
     this.setState(stateMods);
   },
   onFormationChange: function(crusader){
@@ -259,7 +369,20 @@ var CruTagGrid = React.createClass({
         
         var dps = getCrusaderDps(crusader);
         
-        rows.push(<CruTagRow key={crusader.displayName} formationIds={self.state.formationIds} isFormationMode={self.state.formation === "formation"} wikibase={self.props.model.wikibase} crusader={crusader} dps={dps} owned={owned} missionTags={self.props.model.missionTags} mode={self.state.mode} onOwnedChange={self.onOwnedChange.bind(self,crusader)} onFormationChange={self.onFormationChange.bind(self,crusader)} />);
+        rows.push(<CruTagRow key={crusader.displayName} 
+          formationIds={self.state.formationIds} 
+          epMode={self.state.epMode} 
+          enchantmentPoints={self.state.enchantmentPoints[crusader.id]}
+          onEpChange={self.onEpChange.bind(null,crusader.id)}
+          isFormationMode={self.state.formation === "formation"} 
+          wikibase={self.props.model.wikibase} 
+          crusader={crusader} 
+          dps={dps} 
+          owned={owned} 
+          missionTags={self.props.model.missionTags} 
+          mode={self.state.mode} 
+          onOwnedChange={self.onOwnedChange.bind(self,crusader)} 
+          onFormationChange={self.onFormationChange.bind(self,crusader)} />);
       }
     );
     var tagCounts = [];
@@ -282,6 +405,7 @@ var CruTagGrid = React.createClass({
       formationRow=(
         <tr>
           <th><CheckBox checked={this.state.formation === "formation"} onChange={this.onFormationClick} /> Build Formation</th>
+          <th><CheckBox checked={this.state.epMode} onChange={this.onEpClick} /> Track EP</th>
         </tr>
       );
     }
