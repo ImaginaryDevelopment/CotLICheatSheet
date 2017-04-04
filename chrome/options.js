a4bc7893dc628ba52559dd988f8824d9b9898064
bdimpe@gmail <bdimpe@gmail>
2017-04-04 16:07:42 -0400

    added an options page, with permissions request for storage, even though you can't request storage permissions as optional currently

--- chrome/options.js
+++ chrome/options.js
@@ -1,10 +1,9 @@
 // Saves options to chrome.storage.sync.
 function save_options() {
-  var color = document.getElementById('color').value;
-  var likesColor = document.getElementById('like').checked;
+  var autoOpenSite = document.getElementById('site').value;
+//   var likesColor = document.getElementById('like').checked;
   chrome.storage.sync.set({
-    favoriteColor: color,
-    likesColor: likesColor
+    autoOpenSite: autoOpenSite
   }, function() {
     // Update status to let user know options were saved.
     var status = document.getElementById('status');
@@ -18,15 +17,24 @@ function save_options() {
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
+    var status = document.getElementById('status');
+    var btnSave = document.getElementById('save');
+    chrome.permissions.request({
+        permissions:['storage']
+    }, granted =>{
+        if (granted){
+            btnSave.addEventListener('click', save_options);
+        } else {
+            btnSave.style.display = 'none';
+            status.textContent = 'Storage permission is required to save options';
+        }
+    });
   // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get({
-    favoriteColor: 'red',
-    likesColor: true
+    site: 'cotli'
   }, function(items) {
-    document.getElementById('color').value = items.favoriteColor;
-    document.getElementById('like').checked = items.likesColor;
+    document.getElementById('site').value = items.site;
+    // document.getElementById('like').checked = items.likesColor;
   });
 }
 document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-    save_options);
