eae153785fd0ea95ac9d55ce182e05e9d56ce073
ImaginaryDevelopment <imaginarydevelopment@googlemail>
2017-04-16 18:41:41 -0400

    slot numbers for worlds wake laid out

--- js/formationCalc.jsx
+++ js/formationCalc.jsx
@@ -5,37 +5,31 @@
                 <table>
                     <thead></thead>
                     <tbody>
-                        <tr title="row0"><td title="slot0">
-                            <HeroSelect dontSort={true} crusaders={jsonData.crusaders}/>
-                            </td> </tr>
-                        <tr title="row1"><td title="slot1"/><td>
-                            <HeroSelect dontSort={true} crusaders={jsonData.crusaders}/>
-                            </td></tr>
-                        <tr title="row2"><td>
-                            <HeroSelect dontSort={true} crusaders={jsonData.crusaders}/>
-                            </td> 
-                            <td />
-                            <td> <HeroSelect dontSort={true} crusaders={jsonData.crusaders}/> </td> 
+                        <tr title="row0">
+                            <td title="slot0"><HeroSelect dontSort={true} crusaders={jsonData.crusaders}/>0</td>
                         </tr>
-                        <tr title="row3"><td /><td>
-                            <HeroSelect dontSort={true} crusaders={jsonData.crusaders}/>
-                            </td><td /><td>
-                            <HeroSelect dontSort={true} crusaders={jsonData.crusaders}/>
-                            </td>
+                        <tr title="row1">
+                            <td/><td title="slot4"><HeroSelect dontSort={true} crusaders={jsonData.crusaders}/>4</td>
+                        </tr>
+                        <tr title="row2">
+                            <td  title="slot1"> <HeroSelect dontSort={true} crusaders={jsonData.crusaders}/>1</td> 
+                            <td /><td title="slot7"><HeroSelect dontSort={true} crusaders={jsonData.crusaders}/>7</td> 
+                        </tr>
+                        <tr title="row3">
+                            <td /><td title="slots5"><HeroSelect dontSort={true} crusaders={jsonData.crusaders}/>5</td>
+                            <td /><td title="slot9"><HeroSelect dontSort={true} crusaders={jsonData.crusaders}/>9</td>
 
                         </tr>
                         <tr title="row4">
-                            <td> <HeroSelect dontSort={true} crusaders={jsonData.crusaders}/> </td> 
+                            <td title="slot2"> <HeroSelect dontSort={true} crusaders={jsonData.crusaders}/>2</td> 
                             <td />
-                            <td> <HeroSelect dontSort={true} crusaders={jsonData.crusaders}/> </td> 
-                            </tr>
-                        <tr title="row5"><td /><td>
-                            <HeroSelect dontSort={true} crusaders={jsonData.crusaders}/>
-                            </td>
-                            </tr>
-                        <tr title="row6"><td>
-                            <HeroSelect dontSort={true} crusaders={jsonData.crusaders}/>
-                            </td> 
+                            <td title="slot8"><HeroSelect dontSort={true} crusaders={jsonData.crusaders}/>8</td> 
+                        </tr>
+                        <tr title="row5">
+                            <td /><td title="slot6"><HeroSelect dontSort={true} crusaders={jsonData.crusaders}/>6</td>
+                        </tr>
+                        <tr title="row6">
+                            <td title="slot3"><HeroSelect dontSort={true} crusaders={jsonData.crusaders}/>3</td> 
                         </tr>
                 </tbody>
                 </table>
